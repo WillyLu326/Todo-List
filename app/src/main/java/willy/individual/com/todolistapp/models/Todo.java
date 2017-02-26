d99d80e652de4a04568427d2b90fa33a05e5dc41
WillyLu326 <luzh0326@gmail.com>
2017-02-26 13:17:35 -0500

    Create Todo Model

---
+++ app/src/main/java/willy/individual/com/todolistapp/models/Todo.java
@@ -0,0 +1,12 @@
+package willy.individual.com.todolistapp.models;
+
+public class Todo {
+
+    public boolean done;
+
+    public String text;
+
+    public Todo(String text) {
+        this.text = text;
+    }
+}
