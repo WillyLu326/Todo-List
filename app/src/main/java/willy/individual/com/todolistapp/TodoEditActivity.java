e32f52fa988a954c717305b66def3c88a5631a4d
WillyLu326 <luzh0326@gmail.com>
2017-03-01 22:40:31 -0500

    Support bar working

--- app/src/main/java/willy/individual/com/todolistapp/TodoEditActivity.java
+++ app/src/main/java/willy/individual/com/todolistapp/TodoEditActivity.java
@@ -3,6 +3,8 @@ package willy.individual.com.todolistapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class TodoEditActivity extends AppCompatActivity {
@@ -14,5 +16,28 @@ public class TodoEditActivity extends AppCompatActivity {
 
         getSupportActionBar().setElevation(0);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home :
+                finish();
+                return true;
+            case R.id.save_icon :
+                saveAndExit();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveAndExit() {
+        finish();
     }
 }
