967b12ed370d4254c45e44ef3016dd21a0e6b13e
WillyLu326 <luzh0326@gmail.com>
2017-03-01 21:45:41 -0500

    Create TodoEditActivity

---
+++ app/src/main/java/willy/individual/com/todolistapp/TodoEditActivity.java
@@ -0,0 +1,19 @@
+package willy.individual.com.todolistapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhenglu on 3/1/17.
+ */
+
+public class TodoEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_edit);
+
+    }
+}
